d193cc2e1dd6f015b8d387f3fe7e668e8cd2179a
Julian Grey <julianrgrey@gmail.com>
2015-06-19 00:27:05 +0100

    Add new script handling the game's GUI aspects

---
+++ GUIScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+    public GameControl gameControlScript;
+
+    public Canvas mainMenuCanvas;
+    public Canvas gameplayCanvas;
+    public Canvas howToPlayCanvas;
+    public Canvas pauseCanvas;
+
+    private bool howToPlay;
+    private bool settings;
+
+    void Start() {
+        gameControlScript = GameObject.Find("Actions").GetComponent<GameControl>();
+    }
+
+    public void StartGame() {
+        mainMenuCanvas.enabled = false;
+        gameplayCanvas.enabled = true;
+        Application.LoadLevel("Level_01");
+    }
+
+    public void PauseGame() {
+        gameControlScript.LevelPause();
+        pauseCanvas.enabled = true;
+    }
+
+    public void ResumeGame() {
+        gameControlScript.LevelPause();
+        pauseCanvas.enabled = false;
+    }
+
+    public void HowToPlay() {
+        howToPlay = true;
+    }
+
+    public void ReturnToMenu() {
+        if(settings) {
+            settings = false;
+        }
+        else if(howToPlay) {
+            howToPlay = false;
+        }
+    }
+
+    public void GoToMainMenu() {
+        Destroy(GameObject.Find("AudioHandler"));
+        Destroy(GameObject.Find("CanvasHandler"));
+        Destroy(GameObject.Find("GUIHandler"));
+        Destroy(GameObject.Find("TimerHandler"));
+        Application.LoadLevel("Frontend");
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    void Update() {
+        if(howToPlay) {
+            howToPlayCanvas.enabled = true;
+        }
+        else {
+            howToPlayCanvas.enabled = false;
+        }
+        if(pauseCanvas.enabled) {
+            gameplayCanvas.enabled = false;
+        }
+        else {
+            gameplayCanvas.enabled = true;
+        }
+    }
+}
