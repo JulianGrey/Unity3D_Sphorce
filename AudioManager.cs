369ffe9da31be14b993d21da364dc0c4bfa2cd37
Julian Grey <julianrgrey@gmail.com>
2015-06-19 16:53:31 +0100

    Add control for background music

--- AudioManager.cs
+++ AudioManager.cs
@@ -3,4 +3,18 @@ using System.Collections;
 
 public class AudioManager : MonoBehaviour {
     public AudioClip[] gameSounds;
+    private GameControl gameControlScript;
+
+    void Update() {
+        if(gameControlScript != GameObject.Find("Level")) {
+            if(GameObject.Find("Level") != null) {
+                gameControlScript = GameObject.Find("Level").GetComponent<GameControl>();
+            }
+        }
+        if(gameControlScript != null) {
+            if(gameControlScript.gameOver) {
+                GetComponent<AudioSource>().Stop();
+            }
+        }
+    }
 }
