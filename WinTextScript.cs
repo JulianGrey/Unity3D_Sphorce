23378fae79d515af43509b7f34ccc12a777e5009
Julian Grey <julianrgrey@gmail.com>
2015-06-21 20:57:02 +0100

    New scripts handling the text for level complete screen

---
+++ WinTextScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WinTextScript : MonoBehaviour {
+    GameControl gameControlScript;
+    Text text;
+
+    void Awake() {
+        text = GetComponent<Text>();
+    }
+
+    void Update() {
+        if(gameControlScript != GameObject.Find("Level")) {
+            if(GameObject.Find("Level") != null) {
+                gameControlScript = GameObject.Find("Level").GetComponent<GameControl>();
+            }
+        }
+        if(gameControlScript != null) {
+            if(gameControlScript.nextLevel == "Frontend") {
+                text.text = "You have completed Sphorce.\nTouch the screen to go to the main menu.";
+            }
+            else {
+                text.text = "Touch the screen to continue to the next level";
+            }
+        }
+    }
+}
---
+++ WinTitleScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WinTitleScript : MonoBehaviour {
+    GameControl gameControlScript;
+    Text text;
+
+    void Awake() {
+        text = GetComponent<Text>();
+    }
+
+    void Update() {
+        if(gameControlScript != GameObject.Find("Level")) {
+            if(GameObject.Find("Level") != null) {
+                gameControlScript = GameObject.Find("Level").GetComponent<GameControl>();
+            }
+        }
+        if(gameControlScript != null) {
+            if(gameControlScript.nextLevel == "Frontend") {
+                text.text = "CONGRATULATIONS!!";
+            }
+            else {
+                text.text = "LEVEL COMPLETE!";
+            }
+        }
+    }
+}
